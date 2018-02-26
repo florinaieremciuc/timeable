572b494b3f1c0248c204532e5b3acceb3edaead1
FlorinaIe <florina.ieremciuc@gmail.com>
2018-02-26 20:44:26 +0200

    encrypt password

--- store.js
+++ store.js
@@ -1,11 +1,29 @@
 const knex = require("knex")(require("./knexfile"));
+const crypto = require("crypto"); // native node package
+
+const saltHashPassword = password => {
+  const salt = randomString();
+  const hash = crypto.createHmac("sha512", salt).update(password);
+
+  return {
+    salt,
+    hash: hash.digest("hex")
+  };
+};
+
+const randomString = () => {
+  return crypto.randomBytes(4).toString("hex");
+};
 
 module.exports = {
+  saltHashPassword,
   createUser({ username, password }) {
-    console.log(`Add user ${username} w pass ${password}`);
+    console.log(`Add user ${username}`);
+    const { salt, hash } = saltHashPassword(password);
     return knex("user").insert({
-      username,
-      password
+      salt,
+      encrypted_password: hash,
+      username
     });
   }
 };
