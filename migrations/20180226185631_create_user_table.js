6859159d7b4bc570a2a859714e223d0014c48535
florinaieremciuc <florina.ieremciuc@gmail.com>
2018-04-15 22:51:49 +0300

    add unique keys

--- migrations/20180226185631_create_user_table.js
+++ migrations/20180226185631_create_user_table.js
@@ -1,12 +1,18 @@
 exports.up = function(knex, Promise) {
   return knex.schema.createTable("users", t => {
     t.increments("id").primary();
-    t.string("username").notNullable();
+    t
+      .string("username")
+      .notNullable()
+      .unique();
     t.string("password").notNullable();
     t.timestamps(false, true);
     t.string("first_name").notNullable();
     t.string("last_name").notNullable();
-    t.string("email").notNullable();
+    t
+      .string("email")
+      .notNullable()
+      .unique();
     t.string("phone").notNullable();
     t.enu("role", [
       "teamlead",
--- migrations/20180315231521_create_team.js
+++ migrations/20180315231521_create_team.js
@@ -1,7 +1,10 @@
 exports.up = function(knex, Promise) {
   return knex.schema.createTable("teams", t => {
     t.increments("id").primary();
-    t.string("name").notNullable();
+    t
+      .string("name")
+      .notNullable()
+      .unique();
   });
 };
 
