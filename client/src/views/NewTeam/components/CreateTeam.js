786c82852ea940685643e6607f3c841ab7728d53
florinaieremciuc <florina.ieremciuc@gmail.com>
2018-04-24 00:27:58 +0300

    refactor create team component - add members to team

--- client/src/views/NewTeam/components/CreateTeam.js
+++ client/src/views/NewTeam/components/CreateTeam.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { connect } from "react-redux";
-import { Form, Input, Button } from "semantic-ui-react";
+import { Form, Input, Button, Select } from "semantic-ui-react";
 import { Redirect } from "react-router-dom";
 
 import { createTeamAttempt } from "../actions";
@@ -10,26 +10,104 @@ class CreateTeamForm extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      min: 0,
+      max: 8,
+      inputCount: 1,
       team: "",
+      members: [],
       redirect: false
     };
+
+    this.addInput = this.addInput.bind(this);
+    this.addMoreInputs = this.addMoreInputs.bind(this);
+    this.removeInputs = this.removeInputs.bind(this);
+
+    this.handleAddMember = this.handleAddMember.bind(this);
     this.handleChangeName = this.handleChangeName.bind(this);
+    this.handleChangeRole = this.handleChangeRole.bind(this);
     this.submit = this.submit.bind(this);
   }
+
   componentWillReceiveProps(nextProps) {
     nextProps.newteamid && this.setState({ redirect: !this.state.redirect });
   }
+
+  addMoreInputs() {
+    this.state.inputCount < this.state.max &&
+      this.setState({ inputCount: this.state.inputCount + 1 });
+  }
+
+  removeInputs() {
+    this.setState({ inputCount: this.state.inputCount - 1 });
+    this.state.members.pop();
+  }
+
+  addInput(i) {
+    return (
+      <div key={i}>
+        <Form.Field
+          control={Input}
+          name="email"
+          type="email"
+          placeholder="Type in member's email address"
+          onBlur={event => this.handleAddMember(event, i)}
+          required
+        />
+        <Form.Field
+          control={Select}
+          name="role"
+          placeholder="Add member's role"
+          options={[
+            // { key: "teamlead", text: "Team lead", value: "Team lead" },
+            { key: "frontend", text: "Front end", value: "Front end" },
+            { key: "backend", text: "Back end", value: "Back end" },
+            { key: "tester", text: "Tester", value: "Tester" },
+            { key: "sysadmin", text: "Sys admin", value: "Sys admin" }
+          ]}
+          onChange={event => this.handleChangeRole(event, i)}
+          required
+        />
+      </div>
+    );
+  }
+
+  inputs() {
+    var rows = [];
+    var inputCount = this.state.inputCount;
+    for (var i = 0; i < inputCount; i++) {
+      rows.push(this.addInput(i));
+    }
+    return rows;
+  }
+
   handleChangeName(text) {
     this.setState({ name: text.target.value });
   }
+  handleAddMember(text, index) {
+    const members = Object.assign(this.state.members);
+    const email = text.target.value.replace(/\s/g, "");
+    email.includes("@") && email.includes(".")
+      ? members.push({ index, email })
+      : null;
+    this.setState({ members: members });
+  }
+  handleChangeRole(event, index) {
+    const members = Object.assign(this.state.members);
+    members.map(member => {
+      if (member.index === index)
+        member.role = event.target.innerText.replace(/\s/g, "").toLowerCase();
+    });
+    this.setState({ members });
+  }
+
   async submit() {
-    await this.props.createTeamAttempt(this.state.name);
+    await this.props.createTeamAttempt(this.state.name, this.state.members);
   }
   render() {
     if (this.state.redirect) {
       return <Redirect to="/new_user" />;
     }
-
+    console.log(this.state);
     return (
       <Form onSubmit={this.submit}>
         <Form.Group widths="equal">
@@ -42,6 +120,19 @@ class CreateTeamForm extends React.Component {
             placeholder="Name"
             onChange={event => this.handleChangeName(event)}
             required
+          />
+          {this.inputs()}
+          <Form.Field
+            control={Button}
+            content="Add member"
+            onClick={this.addMoreInputs}
+            disabled={this.state.inputCount >= this.state.max}
+          />
+          <Form.Field
+            control={Button}
+            content="Remove member"
+            onClick={this.removeInputs}
+            disabled={this.state.inputCount <= this.state.min}
           />
         </Form.Group>
         <Form.Field
