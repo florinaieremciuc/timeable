28a36f328830c0c95837e8e1571c7f9cbee63b79
florinaieremciuc <florina.ieremciuc@gmail.com>
2018-04-24 22:57:42 +0300

    config mailtrap for teams

--- routes/teams.js
+++ routes/teams.js
@@ -1,5 +1,6 @@
 var express = require("express");
 var router = express.Router();
+var nodemailer = require("nodemailer");
 
 var teamsController = require("../controllers/teams");
 
@@ -25,6 +26,51 @@ router.post("/create_team", (req, res) => {
     .create({ name: req.body.name })
     .then(team => res.send(team))
     .catch(err => res.send(err));
+});
+
+/* Send members emails to join the team */
+router.post("/add_members", (req, res) => {
+  nodemailer
+    .createTestAccount((err, account) => {
+      // create reusable transporter object using the default SMTP transport
+      let transporter = nodemailer.createTransport({
+        host: "smtp.mailtrap.io",
+        port: 2525,
+        auth: {
+          user: "deed36bfaea27e",
+          pass: "7730a5a5f0a126"
+        }
+      });
+
+      req.body.members.map(member => {
+        // setup email data with unicode symbols
+        let mailOptions = {
+          from: '"Timeable 👻" <admin@timeable.com>', // sender address
+          to: member.email, // list of receivers
+          subject: `Join Timeable`, // Subject line
+          text: `Hello! Some douche wants you in its team. Join here: yolo/id`, // plain text body
+          html: `<h3>Hi there!</h3><br/> 
+            <p>Some douche called ${req.body.teamLead} 
+            wants you to join their team. <br/>
+            <a href="http://localhost:500/new_users/${req.body.team}/${
+            member.role
+          }">
+            Click here!
+            </a>.
+            <br/> Regards, <br/> <em>Timeable Team</em></p>` // html body
+        };
+
+        transporter.sendMail(mailOptions, (error, info) => {
+          if (error) {
+            return console.log(error);
+          }
+          console.log("Message sent: %s", info.messageId);
+          console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+        });
+      });
+    })
+    .then(response => res.send(response))
+    .catch(error => res.send(error));
 });
 
 // TO TEST
