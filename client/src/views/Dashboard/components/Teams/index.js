8ec52521ed35da722809489aeab549e4b24e864e
florinaieremciuc <florina.ieremciuc@gmail.com>
2018-04-25 22:03:55 +0300

    add members & send invites

--- client/src/views/Dashboard/components/Teams/index.js
+++ client/src/views/Dashboard/components/Teams/index.js
@@ -1,9 +1,142 @@
-import React from "react";
+import React from 'react';
+import { connect } from 'react-redux';
+import { Container, Form, Input, Button, Select } from 'semantic-ui-react';
+
+import { addMembers } from '../../../../services/Api';
+import { getId as getTeam } from '../../../NewTeam/reducer';
+import { getUserData as getUser } from '../../../Login/reducers';
 
 class Teams extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      min: 0,
+      max: 8,
+      inputCount: 1,
+      members: [],
+      redirect: false,
+    };
+
+    this.addInput = this.addInput.bind(this);
+    this.addMoreInputs = this.addMoreInputs.bind(this);
+    this.removeInputs = this.removeInputs.bind(this);
+
+    this.handleAddMember = this.handleAddMember.bind(this);
+    this.handleChangeRole = this.handleChangeRole.bind(this);
+    this.submit = this.submit.bind(this);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.newteamid) {
+      this.setState({ redirect: !this.state.redirect });
+    }
+  }
+
+  addMoreInputs() {
+    if (this.state.inputCount < this.state.max) {
+      this.setState({ inputCount: this.state.inputCount + 1 });
+    }
+  }
+
+  removeInputs() {
+    this.setState({ inputCount: this.state.inputCount - 1 });
+    this.state.members.pop();
+  }
+  addInput(i) {
+    return (
+      <div key={i}>
+        <Form.Field
+          control={Input}
+          name="email"
+          type="email"
+          placeholder="Type in member's email address"
+          onBlur={event => this.handleAddMember(event, i)}
+          required
+        />
+        <Form.Field
+          control={Select}
+          name="role"
+          placeholder="Add member's role"
+          options={[
+            { key: 'frontend', text: 'Front end', value: 'Front end' },
+            { key: 'backend', text: 'Back end', value: 'Back end' },
+            { key: 'tester', text: 'Tester', value: 'Tester' },
+            { key: 'sysadmin', text: 'Sys admin', value: 'Sys admin' },
+          ]}
+          onChange={event => this.handleChangeRole(event, i)}
+          required
+        />
+      </div>
+    );
+  }
+
+  inputs() {
+    const rows = [];
+    const { inputCount } = this.state;
+    for (let i = 0; i < inputCount; i++) {
+      rows.push(this.addInput(i));
+    }
+    return rows;
+  }
+
+  handleAddMember(text, index) {
+    const members = Object.assign(this.state.members);
+    const email = text.target.value.replace(/\s/g, '');
+    email.includes('@') && email.includes('.') ? members.push({ index, email }) : null;
+    this.setState({ members });
+  }
+  handleChangeRole(event, index) {
+    const members = Object.assign(this.state.members);
+    members.map((member) => {
+      if (member.index === index) {
+        member.role = event.target.innerText.replace(/\s/g, '').toLowerCase();
+      }
+    });
+    this.setState({ members });
+  }
+
+  async submit() {
+    const teamLeadFullName = this.props.user.firstname + ' ' + this.props.user.lastname;
+    await addMembers(this.props.team, teamLeadFullName, this.state.members);
+  }
+
   render() {
-    return <h1>Team</h1>;
+    return (
+      <Container>
+        <h1>Team</h1>
+        <Form onSubmit={this.submit}>
+          <Form.Group widths="equal">
+            {this.inputs()}
+            <Form.Field
+              control={Button}
+              content="Add member"
+              onClick={this.addMoreInputs}
+              disabled={this.state.inputCount >= this.state.max}
+            />
+            <Form.Field
+              control={Button}
+              content="Remove member"
+              onClick={this.removeInputs}
+              disabled={this.state.inputCount <= this.state.min}
+            />
+          </Form.Group>
+          <Form.Field
+            control={Button}
+            content="Send"
+            id="submit"
+            type="submit"
+            color="teal"
+            compact
+            onClick={this.submit}
+          />
+        </Form>
+      </Container>
+    );
   }
 }
 
-export default Teams;
+const mapStateToProps = state => ({
+  team: getTeam(state.team),
+  user: getUser(state.user),
+});
+export default connect(mapStateToProps, null)(Teams);
--- routes/teams.js
+++ routes/teams.js
@@ -1,11 +1,12 @@
-var express = require("express");
-var router = express.Router();
-var nodemailer = require("nodemailer");
+const express = require('express');
 
-var teamsController = require("../controllers/teams");
+const router = express.Router();
+const nodemailer = require('nodemailer');
+
+const teamsController = require('../controllers/teams');
 
 /* GET list of teams. */
-router.get("/", (req, res) => {
+router.get('/', (req, res) => {
   teamsController
     .getAll()
     .then(teams => res.send(teams))
@@ -13,7 +14,7 @@ router.get("/", (req, res) => {
 });
 
 /* GET a team */
-router.get("/:id", (req, res) => {
+router.get('/:id', (req, res) => {
   teamsController
     .getOne(req.params.id)
     .then(team => res.send(team))
@@ -21,7 +22,7 @@ router.get("/:id", (req, res) => {
 });
 
 /* CREATE a team */
-router.post("/create_team", (req, res) => {
+router.post('/create_team', (req, res) => {
   teamsController
     .create({ name: req.body.name })
     .then(team => res.send(team))
@@ -29,68 +30,63 @@ router.post("/create_team", (req, res) => {
 });
 
 /* Send members emails to join the team */
-router.post("/add_members", (req, res) => {
-  nodemailer
-    .createTestAccount((err, account) => {
-      // create reusable transporter object using the default SMTP transport
-      let transporter = nodemailer.createTransport({
-        host: "smtp.mailtrap.io",
-        port: 2525,
-        auth: {
-          user: "deed36bfaea27e",
-          pass: "7730a5a5f0a126"
-        }
-      });
-
-      req.body.members.map(member => {
-        // setup email data with unicode symbols
-        let mailOptions = {
-          from: '"Timeable 👻" <admin@timeable.com>', // sender address
-          to: member.email, // list of receivers
-          subject: `Join Timeable`, // Subject line
-          text: `Hello! Some douche wants you in its team. Join here: yolo/id`, // plain text body
-          html: `<h3>Hi there!</h3><br/> 
-            <p>Some douche called ${req.body.teamLead} 
+router.post('/add_members', (req, res) => {
+  nodemailer.createTestAccount((err, account) => {
+    // create reusable transporter object using the default SMTP transport
+    const transporter = nodemailer.createTransport({
+      host: 'smtp.mailtrap.io',
+      port: 2525,
+      auth: {
+        user: 'deed36bfaea27e',
+        pass: '7730a5a5f0a126',
+      },
+    });
+    req.body.members.map((member) => {
+      // setup email data with unicode symbols
+      const mailOptions = {
+        from: '"Timeable 👻" <admin@timeable.com>', // sender address
+        to: member.email, // list of receivers
+        subject: 'Join Timeable', // Subject line
+        text: 'Hello! Some douche wants you in its team. Join here: yolo/id', // plain text body
+        html: `<h3>Hi there!</h3><br/> 
+            <p>Some douche called <b>${req.body.teamLead}</b> 
             wants you to join their team. <br/>
-            <a href="http://localhost:500/new_users/${req.body.team}/${
-            member.role
-          }">
+            <a href="http://localhost:5000/new_user/${req.body.team}/${member.role}">
             Click here!
             </a>.
-            <br/> Regards, <br/> <em>Timeable Team</em></p>` // html body
-        };
+            <br/> Regards, <br/> <em>Timeable Team</em></p>`, // html body
+      };
 
-        transporter.sendMail(mailOptions, (error, info) => {
-          if (error) {
-            return console.log(error);
-          }
-          console.log("Message sent: %s", info.messageId);
-          console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
-        });
+      transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+          return console.log(error);
+        }
+        console.log('Message sent: %s', info.messageId);
+        console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+        // return null;
       });
-    })
-    .then(response => res.send(response))
-    .catch(error => res.send(error));
+      return null;
+    });
+  });
 });
 
 // TO TEST
 /* UPDATE a team */
-router.get("/update/:id/:name", (req, res) => {
+router.get('/update/:id/:name', (req, res) => {
   teamsController
     .update(req.params.id, req.params.name)
     .then(
       // response is gt 0 if it finds a team with the given id
-      response => (response > 0 ? res.sendStatus(200) : res.sendStatus(404))
-    )
-    .catch(err => console.log("Error: ", err));
+      response => (response > 0 ? res.sendStatus(200) : res.sendStatus(404)))
+    .catch(err => console.log('Error: ', err));
 });
 
 /* DELETE a team */
-router.get("/delete/:id", (req, res) => {
+router.get('/delete/:id', (req, res) => {
   teamsController
     .delete(req.params.id)
     .then(() => res.sendStatus(200))
-    .catch(err => console.log("Error: ", err));
+    .catch(err => console.log('Error: ', err));
 });
 
 module.exports = router;
