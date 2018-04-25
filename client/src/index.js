dc1aed60d9a27d27da1eac214bbc77d83a879262
florinaieremciuc <florina.ieremciuc@gmail.com>
2018-04-25 22:05:20 +0300

    add route

--- client/src/index.js
+++ client/src/index.js
@@ -1,19 +1,19 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { Provider } from "react-redux";
-import { PersistGate } from "redux-persist/integration/react";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
-import { Loader } from "semantic-ui-react";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { Loader } from 'semantic-ui-react';
 
-import "./index.css";
-import registerServiceWorker from "./registerServiceWorker";
-import { store, persistor } from "./store";
+import './index.css';
+import registerServiceWorker from './registerServiceWorker';
+import { store, persistor } from './store';
 
-import App from "./App/App";
-import NewTeam from "./views/NewTeam";
-import Login from "./views/Login";
-import Register from "./views/Register";
-import Dashboard from "./views/Dashboard";
+import App from './App/App';
+import NewTeam from './views/NewTeam';
+import Login from './views/Login';
+import Register from './views/Register';
+import Dashboard from './views/Dashboard';
 
 ReactDOM.render(
   <Provider store={store}>
@@ -24,8 +24,8 @@ ReactDOM.render(
           <Route exact path="/new_team" component={NewTeam} />
           <Route exact path="/login/:teamId" component={Login} />
           <Route exact path="/login" component={Login} />
+          <Route exact path="/new_user/:teamId/:role" component={Register} />
           <Route exact path="/new_user/:teamId" component={Register} />
-          <Route exact path="/new_user" component={Register} />
           <Route exact path="/projects" component={App} />
           <Route exact path="/teams" component={App} />
           <Route exact path="/events" component={App} />
@@ -33,6 +33,6 @@ ReactDOM.render(
       </BrowserRouter>
     </PersistGate>
   </Provider>,
-  document.getElementById("root")
+  document.getElementById('root'),
 );
 registerServiceWorker();
