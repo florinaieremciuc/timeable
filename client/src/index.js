4e925f5e13a81a127ffd051178c9d2412ebf4b6f
florinaieremciuc <florina.ieremciuc@gmail.com>
2018-04-26 00:27:11 +0300

    add profile

--- client/src/index.js
+++ client/src/index.js
@@ -13,7 +13,7 @@ import App from './App/App';
 import NewTeam from './views/NewTeam';
 import Login from './views/Login';
 import Register from './views/Register';
-import Dashboard from './views/Dashboard';
+import Profile from './views/Profile';
 
 ReactDOM.render(
   <Provider store={store}>
@@ -22,13 +22,13 @@ ReactDOM.render(
         <Switch>
           <Route exact path="/" component={App} />
           <Route exact path="/new_team" component={NewTeam} />
-          <Route exact path="/login/:teamId" component={Login} />
-          <Route exact path="/login" component={Login} />
           <Route exact path="/new_user/:teamId/:role" component={Register} />
           <Route exact path="/new_user/:teamId" component={Register} />
+          <Route exact path="/login/:teamId" component={Login} />
           <Route exact path="/projects" component={App} />
           <Route exact path="/teams" component={App} />
           <Route exact path="/events" component={App} />
+          <Route exact path="/profile/:userId" component={Profile} />
         </Switch>
       </BrowserRouter>
     </PersistGate>
