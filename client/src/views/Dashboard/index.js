489ea8359e6526a140e2d978df09458f2822ac3d
florinaieremciuc <florina.ieremciuc@gmail.com>
2018-05-01 17:56:01 +0300

    refactor symbols

--- client/src/views/Dashboard/index.js
+++ client/src/views/Dashboard/index.js
@@ -1,12 +1,13 @@
-import React from "react";
-import { Container, Sidebar, Button, Icon } from "semantic-ui-react";
+import React from 'react';
+import { Sidebar, Button } from 'semantic-ui-react';
+import PropTypes from 'prop-types';
 
-import Projects from "./components/Projects";
-import Teams from "./components/Teams";
-import Events from "./components/Events";
-import Menu from "../../components/Menu";
+import Projects from './components/Projects';
+import Teams from './components/Teams';
+import Events from './components/Events';
+import Menu from '../../components/Menu';
 
-import "./styles.css";
+import './styles.css';
 
 class Dashboard extends React.Component {
   constructor(props) {
@@ -15,23 +16,25 @@ class Dashboard extends React.Component {
     this.toggleVisibility = this.toggleVisibility.bind(this);
     this.setView = this.setView.bind(this);
   }
+
+  setView() {
+    switch (this.props.path) {
+    case '/projects':
+      return <Projects />;
+    case '/teams':
+      return <Teams />;
+    case '/events':
+      return <Events />;
+    case '/':
+      return null;
+    default:
+      return <h1>Not found</h1>;
+    }
+  }
   toggleVisibility() {
     this.setState({ visible: !this.state.visible });
   }
-  setView() {
-    switch (this.props.path) {
-      case "/projects":
-        return <Projects />;
-      case "/teams":
-        return <Teams />;
-      case "/events":
-        return <Events />;
-      case "/":
-        return null;
-      default:
-        return <h1>Not found</h1>;
-    }
-  }
+
   render() {
     const { visible } = this.state;
 
@@ -40,12 +43,13 @@ class Dashboard extends React.Component {
         <Button className="side-menu" onClick={this.toggleVisibility} />
         <Sidebar.Pushable as="div">
           <Menu visible={visible} />
-          <Sidebar.Pusher className="side-menu">
-            {this.setView()}
-          </Sidebar.Pusher>
+          <Sidebar.Pusher className="side-menu">{this.setView()}</Sidebar.Pusher>
         </Sidebar.Pushable>
       </div>
     );
   }
 }
 export default Dashboard;
+Dashboard.propTypes = {
+  path: PropTypes.string.isRequired,
+};
