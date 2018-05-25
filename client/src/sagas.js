d2bd01c28eb4b4ba6014337e9159b38f0eea6b7c
florinaieremciuc <florina.ieremciuc@gmail.com>
2018-05-25 19:49:42 +0300

    refacotor project saga

--- client/src/sagas.js
+++ client/src/sagas.js
@@ -1,6 +1,6 @@
 import { fork } from 'redux-saga/effects';
 
-import watchGetProjectAttempt from './State/Projects/get/watcher';
+import watchGetProjectsAttempt from './State/Projects/get/watcher';
 import watchCreateProjectAttempt from './State/Projects/create/watcher';
 import watchGetTeamsAttempt from './State/Teams/get/watcher';
 import watchCreateTeamAttempt from './State/Teams/create/watcher';
@@ -9,7 +9,7 @@ import watchLoginAttempt from './State/Users/login/watcher';
 
 // start the daemons
 export default function* root() {
-  yield fork(watchGetProjectAttempt);
+  yield fork(watchGetProjectsAttempt);
   yield fork(watchCreateProjectAttempt);
   yield fork(watchGetTeamsAttempt);
   yield fork(watchCreateTeamAttempt);
