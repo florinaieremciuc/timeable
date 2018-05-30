647bf19159fd632b8ef55e588d3b53133ea47407
florinaieremciuc <florina.ieremciuc@gmail.com>
2018-05-30 19:40:34 +0300

    add modal w router

--- client/src/index.js
+++ client/src/index.js
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { ModalContainer, ModalRoute } from 'react-router-modal';
 import { Loader } from 'semantic-ui-react';
 
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 import { store, persistor } from './store';
 
+import Tasks from './components/Tasks';
 import App from './App/App';
 import NewTeam from './views/NewTeam';
 import Login from './views/Login';
@@ -19,18 +21,22 @@ ReactDOM.render(
   <Provider store={store}>
     <PersistGate loading={<Loader active />} persistor={persistor}>
       <BrowserRouter>
-        <Switch>
-          <Route exact path="/" component={App} />
-          <Route exact path="/new_team" component={NewTeam} />
-          <Route exact path="/new_user/:teamId/:role" component={Register} />
-          <Route exact path="/new_user/:teamId/:role/:encryptEmail" component={Register} />
-          <Route exact path="/new_user/:teamId" component={Register} />
-          <Route exact path="/login" component={Login} />
-          <Route exact path="/projects" component={App} />
-          <Route exact path="/teams" component={App} />
-          <Route exact path="/events" component={App} />
-          <Route exact path="/profile/:userId" component={Profile} />
-        </Switch>
+        <div>
+          <Switch>
+            <Route exact path="/" component={App} />
+            <Route exact path="/new_team" component={NewTeam} />
+            <Route exact path="/new_user/:teamId/:role" component={Register} />
+            <Route path="/new_user/:teamId/:role/:encryptEmail" component={Register} />
+            <Route exact path="/new_user/:teamId" component={Register} />
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/projects" component={App} />
+            <Route exact path="/teams" component={App} />
+            <Route exact path="/events" component={App} />
+            <Route exact path="/profile/:userId" component={Profile} />
+            <ModalRoute exact path="/tasks/:projectid" component={Tasks} />
+          </Switch>
+          <ModalContainer containerClassName="react-router-modal__container tasks" />
+        </div>
       </BrowserRouter>
     </PersistGate>
   </Provider>,
