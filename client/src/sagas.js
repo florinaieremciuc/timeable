c0d6d4bb58d097f2bde090ea1ec82b2bb85be742
florinaieremciuc <florina.ieremciuc@gmail.com>
2018-06-01 17:31:32 +0300

    update sagas

--- client/src/sagas.js
+++ client/src/sagas.js
@@ -1,26 +1,43 @@
 import { fork } from 'redux-saga/effects';
 
+import {
+  watchUpdateAssigneeAttempt,
+  watchUpdateStatusAttempt,
+  watchUpdateDurationAttempt,
+} from './State/Tasks/update/watcher';
 import watchDeleteTaskAttempt from './State/Tasks/delete/watcher';
 import watchGetTaskAttempt from './State/Tasks/get/watcher';
 import watchCreateTaskAttempt from './State/Tasks/create/watcher';
+
 import watchDeleteProjectAttempt from './State/Projects/delete/watcher';
 import watchGetProjectsAttempt from './State/Projects/get/watcher';
 import watchCreateProjectAttempt from './State/Projects/create/watcher';
+
 import watchGetTeamsAttempt from './State/Teams/get/watcher';
 import watchCreateTeamAttempt from './State/Teams/create/watcher';
+
 import watchRegisterAttempt from './State/Users/register/watcher';
 import watchLoginAttempt from './State/Users/login/watcher';
+import watchGetMembersAttempt from './State/Users/team/watcher';
 
 // start the daemons
 export default function* root() {
+  yield fork(watchUpdateDurationAttempt);
+  yield fork(watchUpdateStatusAttempt);
+  yield fork(watchUpdateAssigneeAttempt);
+
   yield fork(watchDeleteTaskAttempt);
   yield fork(watchGetTaskAttempt);
   yield fork(watchCreateTaskAttempt);
+
   yield fork(watchDeleteProjectAttempt);
   yield fork(watchGetProjectsAttempt);
   yield fork(watchCreateProjectAttempt);
+
   yield fork(watchGetTeamsAttempt);
   yield fork(watchCreateTeamAttempt);
+
+  yield fork(watchGetMembersAttempt);
   yield fork(watchRegisterAttempt);
   yield fork(watchLoginAttempt);
 }
