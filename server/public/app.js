812141a75ce9d163f4095b4dd975d1f39d930581
florinaieremciuc <florina.ieremciuc@gmail.com>
2018-06-23 00:09:18 +0300

    refactor api test script

--- server/public/app.js
+++ server/public/app.js
@@ -1,64 +1,61 @@
-const OrgList = document.querySelector(".organization");
-OrgList.addEventListener("onload", e => {
+const post = (path, data) =>
+  window.fetch(path, {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+  });
+const get = path =>
+  window
+    .fetch(path)
+    .then(response => response)
+    .catch(err => err);
+const OrgList = document.querySelector('.organization');
+
+OrgList.addEventListener('onload', (e) => {
   e.preventDefault();
-  get("/");
+  get('/');
 });
 // const posts = get("/organizations");
 // console.log("...", posts);
 // console.log("...", posts.resolve());
 
-const CreateOrg = document.querySelector(".CreateOrg");
-CreateOrg.addEventListener("submit", e => {
+const CreateOrg = document.querySelector('.CreateOrg');
+CreateOrg.addEventListener('submit', (e) => {
   e.preventDefault();
-  const name = CreateOrg.querySelector(".name").value;
-  console.log("check", name);
-  post("/organizations/create_organization", { name });
+  const name = CreateOrg.querySelector('.name').value;
+  console.log('check', name);
+  post('/organizations/create_organization', { name });
 });
 
-const CreateUser = document.querySelector(".CreateUser");
-CreateUser.addEventListener("submit", e => {
+const CreateUser = document.querySelector('.CreateUser');
+CreateUser.addEventListener('submit', (e) => {
   e.preventDefault();
-  const username = CreateUser.querySelector(".username").value;
-  const password = CreateUser.querySelector(".password").value;
-  const first_name = CreateUser.querySelector(".firstname").value;
-  const last_name = CreateUser.querySelector(".lastname").value;
-  const email = CreateUser.querySelector(".email").value;
-  const phone = CreateUser.querySelector(".phone").value;
-  post("/users/create_user", {
+  const username = CreateUser.querySelector('.username').value;
+  const password = CreateUser.querySelector('.password').value;
+  const first_name = CreateUser.querySelector('.firstname').value;
+  const last_name = CreateUser.querySelector('.lastname').value;
+  const email = CreateUser.querySelector('.email').value;
+  const phone = CreateUser.querySelector('.phone').value;
+  post('/users/create_user', {
     username,
     password,
     first_name,
     last_name,
     email,
-    phone
+    phone,
   });
 });
 
-const Login = document.querySelector(".Login");
-Login.addEventListener("submit", e => {
+const Login = document.querySelector('.Login');
+Login.addEventListener('submit', (e) => {
   e.preventDefault();
-  const username = Login.querySelector(".username").value;
-  const password = Login.querySelector(".password").value;
-  post("/users/login", { username, password }).then(({ status }) => {
-    if (status === 200) alert("Login success");
-    else alert("Login failed");
+  const username = Login.querySelector('.username').value;
+  const password = Login.querySelector('.password').value;
+  post('/users/login', { username, password }).then(({ status }) => {
+    if (status === 200) alert('Login success');
+    else alert('Login failed');
   });
 });
-
-const post = (path, data) => {
-  return window.fetch(path, {
-    method: "POST",
-    headers: {
-      Accept: "application/json",
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify(data)
-  });
-};
-
-const get = path => {
-  return window
-    .fetch(path)
-    .then(response => response)
-    .catch(err => err);
-};
