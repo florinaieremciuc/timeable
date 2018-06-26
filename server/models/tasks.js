a80746dacae191652cd16af79e0602ad3b75fad0
florinaieremciuc <florina.ieremciuc@gmail.com>
2018-06-26 20:48:04 +0300

    refactor tasks requests

--- server/models/tasks.js
+++ server/models/tasks.js
@@ -36,10 +36,15 @@ module.exports = {
         'priority',
         'project',
         'estimate',
-        'assignee',
+        'user_id',
+        'username',
+        'first_name',
+        'last_name',
+        'role',
       )
       .from('tasks')
-      .innerJoin('users', 'tasks.assignee', 'users.id')
+      .innerJoin('users_tasks', 'tasks.id', 'users_tasks.task_id')
+      .innerJoin('users', 'users_tasks.user_id', 'users.id')
       .where('users.team', teamid)
       .then((tasks) => {
         if (!tasks || tasks.length === 0) return { error: 'No tasks assigned' };
@@ -47,31 +52,43 @@ module.exports = {
       })
       .catch(error => error);
   },
-  getUnassigned() {
-    console.log('Get tasks that have not been assigned to ppl');
+  getUnassigned(teamid) {
+    console.log(`Get unassigned tasks from team having id ${teamid}`);
+    const subQuery = knex.select('task_id').from('users_tasks');
     return knex('tasks')
-      .whereNull('asignee')
+      .innerJoin('projects', 'tasks.projects', 'projects.id')
+      .where('projects.team', teamid)
+      .andWhere('tasks.id', 'not in', subQuery)
       .then((tasks) => {
-        if (!tasks) return { error: 'No tasks assigned' };
+        if (!tasks) return { error: 'No unassigned tasks' };
         return tasks;
       });
   },
   getOne(id) {
     console.log(`Get task w id ${id}`);
-    return knex('tasks')
-      .where('id', id)
+    return knex
+      .select(
+        'tasks.id',
+        'tasks.name',
+        'tasks.description',
+        'status',
+        'priority',
+        'estimate',
+        'projects.id',
+      )
+      .from('tasks')
+      .where('tasks.id', id)
       .then(([task]) => {
         if (!task) return { error: 'Task does not exist' };
         return { success: true, task };
       });
   },
   addAssignee(id, assignee) {
-    console.log(`Updating task having id ${id} w assignee ${assignee}`);
-    return knex('tasks')
-      .where('id', id)
-      .update({
-        assignee,
-      });
+    console.log(`Add entry in users_tasks for task having id ${id} w assignee ${assignee}`);
+    return knex('users_tasks').insert({
+      user_id: assignee,
+      task_id: id,
+    });
   },
   addDuration(id, duration) {
     console.log(`Updating task having id ${id} w duration ${duration}`);
